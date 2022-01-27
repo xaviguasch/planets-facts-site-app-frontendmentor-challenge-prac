3988e09d2b652a60904379ba293788d990c0768f
xaviguasch <xaviguasch@gmail.com>
2022-01-27 21:05:35 +0100

    installed react router and added Planet component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
 
+import Planet from './components/Planet'
+
 function App() {
   return (
     <div className='App'>
       <h1>Planets Site</h1>
+
+      <Planet />
     </div>
   )
 }
---
+++ src/components/Planet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Planet.css'
+
+const Planet = () => {
+  return (
+    <div className='Planet'>
+      <h2>Planet</h2>
+    </div>
+  )
+}
+
+export default Planet
