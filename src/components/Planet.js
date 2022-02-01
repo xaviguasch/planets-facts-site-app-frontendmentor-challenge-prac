a82dc6bc27f2f3c07b4a4f6c75ae44402b880038
xaviguasch <xaviguasch@gmail.com>
2022-02-01 20:26:45 +0100

    dynamically rendered data in Planet component based on the state

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -6,9 +6,17 @@ import data from '../data.json'
 import './Planet.css'
 
 const Planet = () => {
+  const [infoDisplay, setInfoDisplay] = useState('overview')
+
+  const handleInfoDisplayChange = (e) => {
+    setInfoDisplay(e.target.value)
+  }
+
   let params = useParams()
 
   const dataPlanet = data.find((item) => item.name.toLowerCase() === params.planetName)
+
+  const { name, images, overview, structure, geology } = dataPlanet
 
   if (dataPlanet) {
     return (
@@ -16,17 +24,66 @@ const Planet = () => {
         <nav>
           <ul>
             <li>
-              <button>Overview</button>
+              <button
+                className={`planet-btn ${infoDisplay === 'overview' ? 'active' : ''}`}
+                onClick={handleInfoDisplayChange}
+                value='overview'
+              >
+                Overview
+              </button>
             </li>
             <li>
-              <button>Structure</button>
+              <button
+                className={`planet-btn ${infoDisplay === 'structure' ? 'active' : ''}`}
+                onClick={handleInfoDisplayChange}
+                value='structure'
+              >
+                Structure
+              </button>
             </li>
             <li>
-              <button>Surface</button>
+              <button
+                className={`planet-btn ${infoDisplay === 'surface' ? 'active' : ''}`}
+                onClick={handleInfoDisplayChange}
+                value='surface'
+              >
+                Surface
+              </button>
             </li>
           </ul>
         </nav>
-        <h1>{params.planetName}</h1>
+
+        {infoDisplay === 'overview' && (
+          <div>
+            <h1>{name}</h1>
+            <p>{overview.content}</p>
+            <p>
+              Source: <a href={overview.source}>Wikipedia</a>
+            </p>
+            <img src={images.planet.replace('./', '/')} alt={name} />
+          </div>
+        )}
+        {infoDisplay === 'structure' && (
+          <div>
+            <h1>{name}</h1>
+            <p>{structure.content}</p>
+            <p>
+              Source: <a href={structure.source}>Wikipedia</a>
+            </p>
+            <img src={images.internal.replace('./', '/')} alt={name} />
+          </div>
+        )}
+        {infoDisplay === 'surface' && (
+          <div>
+            <h1>{name}</h1>
+            <p>{geology.content}</p>
+            <p>
+              Source: <a href={geology.source}>Wikipedia</a>
+            </p>
+            <img src={images.planet.replace('./', '/')} alt={name} />
+            <img src={images.geology.replace('./', '/')} alt={name} />
+          </div>
+        )}
       </div>
     )
   } else {
